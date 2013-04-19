936e714a780cf36b74df7639c24182caa12b8c7f
JuanGarcia <juangrdiaz@gmail.com>
2013-04-19 17:57:03 +0200

    LoginActivity terminada. Ya almacena la cookie en SP y pasa a MainActivity.

--- src/um/cmovil/actividades/MainActivity.java
+++ src/um/cmovil/actividades/MainActivity.java
@@ -4,10 +4,15 @@ import um.cmovil.R;
 import um.cmovil.modelo.Controlador;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+	SharedPreferences formStore;
+	public static final String PREFS_NAME = "MyPrefsFile";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +22,12 @@ public class MainActivity extends Activity {
 		Controlador.setUserAgent("Jara");
 		Controlador.setKey("TestKey");
 		Controlador.setServer("192.168.1.134:8080");
+
+		// File defined in PREFS_NAME
+		formStore = getSharedPreferences(PREFS_NAME, 0);
+
+		Toast.makeText(MainActivity.this, formStore.getString("Cookie", ""),
+				Toast.LENGTH_LONG).show();
 
 	}
 
